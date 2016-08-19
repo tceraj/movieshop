bea25d8880c91f1aee27eda3acd2550db351c8db
Prelang Builder <builder@prelang.com>
2016-08-19 17:05:19 +0000

    Generated model Customer with migration

---
+++ db/migrate/20160819170519_create_customers.rb
@@ -0,0 +1,11 @@
+class CreateCustomers < ActiveRecord::Migration
+  def change
+    create_table :customers do |t|
+      t.string :name
+      t.string :phone
+      t.text :comment
+
+      t.timestamps
+    end
+  end
+end
